48e9f579e5f1c6311e09aa60991238f3c0e5985e
Peter van Grieken <hi@frozenrockets.nl>
2014-03-05 11:18:52 +0100

    Minor fix

--- gruntfile.js
+++ gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
         cssmin: {
             combine: {
                 files: {
-                    './public/css/style.min.css': ['src/css/style.css']
+                    'public/css/style.min.css': ['src/css/style.css']
                 }
             }
         },
