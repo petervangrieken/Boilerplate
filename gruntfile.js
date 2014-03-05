da0f7fc6a19b1261fbd5722301b5bf7e6e117600
Peter van Grieken <hi@frozenrockets.nl>
2014-03-05 12:56:27 +0100

    Added bake to watch

--- gruntfile.js
+++ gruntfile.js
@@ -32,10 +32,21 @@ module.exports = function(grunt) {
               }]
             }
         },
+        bake: {
+            build: {
+                files: {
+                    "public/index.html": "src/index.html"
+                }
+            }
+        },
         watch: {
             css: {
-                files: '**/*.scss',
+                files: ['**/*.scss'],
                 tasks: ['sass', 'cssmin', 'imagemin']
+            },
+            html: {
+                files: ['src/*.html'],
+                tasks: ['bake']
             }
         }
 
